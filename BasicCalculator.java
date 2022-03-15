0ab8eb25878e367307a982cc933df3a9cf37e68e
samcarozzi <100541583+samcarozzi@users.noreply.github.com>
2022-03-14 21:23:17 -0400

    Update BasicCalculator.java

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -8,13 +8,19 @@ public class BasicCalculator {
         System.out.println("Enter second operand: ");
         double second = in.nextDouble();
         System.out.println(" Calculator Menu \n --------------- \n" +
-                "1. Addition \n");
+                "1. Addition \n" +
+                "2. Subtraction \n");
         System.out.println("Which operation do you want to perform?");
+
         int selection = in.nextInt();
         double result;
         //menu with items to select
         if(selection == 1){
             result = first + second;
+            System.out.println("The result of the operation is " + result + ". Goodbye!");
+        }
+        else if(selection == 2){
+            result = first - second;
             System.out.println("The result of the operation is " + result + ". Goodbye!");
         }
     }
