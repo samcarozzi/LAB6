8f68a949846e9cbb13f52e6d7a0c5c3a51073df4
samcarozzi <100541583+samcarozzi@users.noreply.github.com>
2022-03-14 21:15:59 -0400

    Add files via upload

---
+++ BasicCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class BasicCalculator {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        //to create initial vars
+        System.out.print("Enter first operand: ");
+        double first = in.nextDouble();
+        System.out.println("Enter second operand: ");
+        double second = in.nextDouble();
+        System.out.println(" Calculator Menu \n --------------- \n" +
+                "1. Addition \n");
+        System.out.println("Which operation do you want to perform?");
+        int selection = in.nextInt();
+        double result;
+        //menu with items to select
+        if(selection == 1){
+            result = first + second;
+            System.out.println("The result of the operation is " + result + ". Goodbye!");
+        }
+    }
+}
